e888aecbc174643e6bbba6e80262d9905098bc69
Ravish <211128@iiitt.ac.in>
2024-06-14 02:44:04 +0530

    added editable property

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import AddTodo from "@/components/shared/add-todo";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Delete, Trash } from "lucide-react";
+import { Delete, SquareCheckBig, Trash } from "lucide-react";
 // import { useFetch } from "@/hooks/useFetch-hook";
 import { useState } from "react";
 
@@ -15,13 +15,15 @@ export type TodoItem = {
 }
 
 export default function Home() {
+  const [editableTitle, setEditableTitle] = useState("")
+  const [edittodoId, setEditTodoId] = useState<string | number>(-1)
   const [todoData, setTodoData] = useState<TodoItem[]>([])
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<any>(null);
   const [filter, setFilter] = useState("")
   // const [data, loading, error]= useFetch('https://jsonplaceholder.typicode.com/todos')
   const handleFetchTodos = async () => {
-    if(todoData.length>0){
+    if (todoData.length > 0) {
       alert('already fetched todos')
       return;
     }
@@ -31,46 +33,62 @@ export default function Home() {
       const data = await response.json()
       setTodoData(data)
       // console.log(data)
-    } catch (error : any) {
+    } catch (error: any) {
       // console.log(error)
       setError(error)
-    }finally{
+    } finally {
       setLoading(false)
     }
   }
 
   const handleResetTodos = () => setTodoData([])
 
-  if(loading) return <div>Loading...</div>
+  if (loading) return <div>Loading...</div>
 
-  if(error) return <div>error...</div>
+  if (error) return <div>error...</div>
 
   return (
     <main>
       <h1 className="text-center text-3xl">Todo Application</h1>
       <div className="flex justify-between px-6 items-center my-4">
-      <Button variant="outline" onClick={handleFetchTodos}>Fetch Todos</Button>
-      <AddTodo setTodoData={setTodoData} />
-      <Button variant="destructive" onClick={handleResetTodos}>Reset Todos</Button>
+        <Button variant="outline" onClick={handleFetchTodos}>Fetch Todos</Button>
+        <AddTodo setTodoData={setTodoData} />
+        <Button variant="destructive" onClick={handleResetTodos}>Reset Todos</Button>
       </div>
       <div className="flex justify-center items-center mb-4">
         <div className="w-1/4 relative">
-        <Input type="text" value={filter} onChange={(e)=>setFilter(e.target.value)} placeholder="Search Todos" />
-        <Delete onClick={()=>setFilter("")} className={`cursor-pointer absolute right-2 top-1/4 opacity-65 ${filter === "" && "hidden"}`} />
+          <Input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} placeholder="Search Todos" />
+          <Delete onClick={() => setFilter("")} className={`cursor-pointer absolute right-2 top-1/4 opacity-65 ${filter === "" && "hidden"}`} />
         </div>
       </div>
       <div className="grid grid-cols-4 gap-8 p-2">
-      {
-        todoData.filter(todo=>todo.title.toLowerCase().includes(filter.toLowerCase())).map((todoItem) => (
-          <div key={todoItem.id} className="grid grid-cols-2 bg-primary/10 p-4 relative">
-            <p>😎</p>
-            <p>{todoItem.title}</p>
-            <p>{todoItem.userId}</p>
-            <p>{todoItem.completed? "true" : "false"}</p>
-            <Trash onClick={() => setTodoData(todoData.filter((item) => item.id !== todoItem.id))} className="absolute right-1 bottom-1 cursor-pointer" />
-          </div>
-        ))
-      }
+        {
+          todoData.filter(todo => todo.title.toLowerCase().includes(filter.toLowerCase())).map((todoItem) => (
+            <div key={todoItem.id} className="grid grid-cols-2 bg-primary/10 p-4 relative">
+              <p>😎</p>
+              {
+                edittodoId === todoItem.id ? (
+                  <Input type="text" value={editableTitle} onChange={(e)=>setEditableTitle(e.target.value)} />
+                ) : (
+                  <p onClick={()=> {
+                    setEditTodoId(todoItem.id)
+                    setEditableTitle(todoItem.title)
+                  }}>{todoItem.title}</p>
+                )
+              }
+              <p>{todoItem.userId}</p>
+              <p>{todoItem.completed ? "true" : "false"}</p>
+              {
+                edittodoId === todoItem.id ? (
+                  <SquareCheckBig className="cursor-pointer" onClick={() => {setTodoData(todoData.map((item) => item.id === todoItem.id ? {...item, title: editableTitle} : item))
+                setEditTodoId(-1)}} />
+                  ) : (
+                  <Trash onClick={() => setTodoData(todoData.filter((item) => item.id !== todoItem.id))} className="absolute right-1 bottom-1 cursor-pointer" />
+                )
+              }
+            </div>
+          ))
+        }
       </div>
     </main>
   );
